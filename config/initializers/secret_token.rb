db9ff8be09022656fe3f5facd7707ea995b8313a
Eliot Stone <whyriskit@gmail.com>
2014-05-25 14:45:04 -0400

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Waffle::Application.config.secret_key_base = 'd819fd7d182c5ad35dab452b326537f4442c2339b09954311b262f8603c3cf910ac29a88cc015a2344c688080dbcb4f8d9d9bc1420b8aa042531d6c76fda0b05'
