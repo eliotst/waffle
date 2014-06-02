7682550d955af862ffcd5a8bc00052816e277ca8
sschauer <sschauer@fandm.edu>
2014-06-02 11:18:44 -0400

    log in layout
    updated log in layout and trying to fix a link

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Waffle::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/userHome', to: 'static_pages#show', via: 'get'
   root to: 'static_pages#home'
   
   resources :users
