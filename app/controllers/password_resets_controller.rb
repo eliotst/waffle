694fbcff49c2d636ef8db72c6a538573b9a2318d
sschauer <sschauer@fandm.edu>
2014-06-12 14:06:35 -0400

    Password Reset
    finished and functional password reset/forgotten password function

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -16,10 +16,19 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by_auth_token!(params[:id])
     if @user.password_reset_sent_at < 3.hours.ago
       redirect_to new_password_reset_path, :alert => "Password reset has expired."
-    elsif @user.update_attributes(params[:user])
+    elsif @user.update_attributes(user_params)
       redirect_to root_url, :notice => "Password has been reset!"
     else
       render :edit
     end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password,
+         :password_confirmation, :name, :address_line_one, 
+         :address_line_two, :city, :state, :zip_code, 
+         :auth_token, :password_reset_token )
+    end
+
 end
