542dad78560dc8b41f38b55695e1537cbbbc1ed7
sschauer <sschauer@fandm.edu>
2014-06-12 16:47:02 -0400

    layout
    updated layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Signed up! Thank you! 
-        You should receive a validation email shortly."
+      flash[:success] = "Signed up! Thank you!"
+       # You should receive a validation email shortly."
       redirect_to @user
-      @user.send_validation
+      #@user.send_validation
     else
       render "new"
     end
