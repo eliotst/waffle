ccf6469297f4b05cb4bc040b93a09ac12f1605d6
sschauer <sschauer@fandm.edu>
2014-06-17 10:57:30 -0400

    test update
    update

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,10 +1,10 @@
 require "spec_helper"
 
 describe UserMailer do
+  user = User.create(email: 'email@example.com',
+     password: "abc123", password_confirmation: "abc123", auth_token: "asdf23r", valid_token: "234jbhk")
   describe "password_reset" do
-    @user = User.create(email: 'email@example.com',
-     password: "abc123", password_confirmation: "abc123")
-    let(:mail) { UserMailer.password_reset(@user) }
+    let(:mail) { UserMailer.password_reset(user) }
 
     it "renders the headers" do
       mail.subject.should eq("Password Reset")
@@ -14,13 +14,13 @@ describe UserMailer do
 
     it "renders the body" do
       mail.body.encoded.should match("To reset your password, click the URL below. The URL will be live for 3 hours.
-        <%= edit_password_reset_url(@user.auth_token) %>
+        <%= edit_password_reset_url(user.auth_token) %>
         If you did not request your password to be reset, just ignore this email and your password will continue to stay the same.")
     end
   end
 
   describe "validation" do
-    let(:mail) { UserMailer.validation(@user) }
+    let(:mail) { UserMailer.validation(user) }
 
     it "renders the headers" do
       mail.subject.should eq("Validation Email")
@@ -30,7 +30,7 @@ describe UserMailer do
 
     it "renders the body" do
       mail.body.encoded.should match("To validate your account, click the URL below.
-        <%= edit_validation_url(@user.valid_token) %>
+        <%= edit_validation_url(user.valid_token) %>
         You will be redirected to a page for security reasons.")
     end
   end
