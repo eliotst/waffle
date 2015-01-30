4a531e4f6267ee7df8a60fdbc344513bdc068ba6
whyriskit <whyriskit@gmail.com>
2015-01-30 18:42:05 -0500

    more changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,17 @@ Waffle::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.smtp_settings = {
+    :address              => "eliothstone.com",
+
+    :port                 => "25",
+    :domain               => "eliothstone.com",
+
+    :enable_starttls_auto => true,
+    :authentication       => :login,
+    :user_name            => "test@eliothstone.com",
+    :password             => "T3stuser!@$%"
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
